57e23bbb043e8beb300c7a16a30a5232605e3119
gordon tang <gordon.tang@hyperjar.com>
2021-11-01 17:47:33 +0000

    make first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (Gordon Tang)
         </a>
       </header>
     </div>
